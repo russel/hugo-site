507ecafb89816422bef51da42671ab7c31cd3785
Jim Hague <jim.hague@acm.org>
2019-10-12 16:08:35 +0100

    Add Flask module for requiring membership to see CVu.
    The idea is that we configure Apache to direct /login and /logout, plus
    all pages under /journal/cvu to Flask. It will ensure all access to
    /journal/cvu/* latter are logged in and are ACCU members; if not, they
    are redirected to the login page.
    
    I have tested this standalone, but not in the Apache setup, so this
    might not fly.
    
    Also, I've gone with the Python import style the Flask docs use, though
    I don't like from ... import.
    
    To avoid including the database password in the source, this requires a
    configuration /etc/accu/website.cfg (or ~<apache user>/.accu.cfg)
    containing the location of the MySQL database and the password for the
    ACCU Xaraya user. The location defaults to 'localhost', the password to
    in incorrect value.
    
    [database]
    host=localhost
    password=<password>

--- flask/accupassword.py
+++ flask/accupassword.py
@@ -11,9 +11,13 @@ import pymysql
 
 class Checker:
     def __init__(self, dbhost, dbpass):
-        self.db = pymysql.connect(dbhost, 'accuorg_xarad', dbpass, 'accuorg_xar', charset='latin1')
+        self.db = pymysql.connect(host=dbhost,
+                                  user='accuorg_xarad',
+                                  password=dbpass,
+                                  db='accuorg_xar',
+                                  charset='latin1')
 
-    def user(self, username, userpass):
+    def __getuser(self, username, userpass):
         cursor = self.db.cursor()
         try:
             cursor.execute('SELECT xar_pass, xar_name, xar_status FROM xar_roles LEFT JOIN xar_subscriptions USING (xar_uid) WHERE xar_uname=%s', username)
@@ -22,9 +26,17 @@ class Checker:
             m.update(userpass.encode('latin1'))
             if m.hexdigest() != row[0]:
                 return ()
-            return (row[1], row[2] == 1)
+            return (row[1], row[2])
         except:
             return ()
+
+    def user(self, username, userpass):
+        user_info = self.__getuser(username, userpass)
+        return bool(user_info)
+
+    def member(self, username, userpass):
+        user_info = self.__getuser(username, userpass)
+        return bool(user_info) and user_info[1] == 1
 
 def main():
     parser = argparse.ArgumentParser(description='test password library')
@@ -43,9 +55,10 @@ def main():
     args = parser.parse_args()
 
     checker = Checker(args.dbhost, args.dbpass)
-    user = checker.user(args.user, args.passwd)
-    if len(user):
-        print('Name {name}, is member {member}'.format(name=user[0], member=user[1]))
+    if checker.member(args.user, args.passwd):
+        print('User \'{name}\' is ACCU member.'.format(name=args.user))
+    elif checker.user(args.user, args.passwd):
+        print('Name \'{name}\' is ACCU website user.'.format(name=args.user))
     else:
         print('Unknown user or wrong password')
     sys.exit(0)
---
+++ flask/cvu.py
@@ -0,0 +1,95 @@
+from configparser import ConfigParser
+from os.path import expanduser
+from flask import Flask, flash, render_template, redirect, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user, LoginManager
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+from accupassword import Checker
+
+_defaults = {
+    'database': {
+        'host': 'localhost',
+        'password': 'NotTheActualPassword'
+    }
+}
+
+class Config(ConfigParser):
+    def __init__(self, configfile=None):
+        super().__init__()
+        self.read_dict(_defaults)
+        if configfile:
+            with open(configfile) as f:
+                self.read_file(f)
+        else:
+            self.read([expanduser('~/.accu.cfg'),
+                     '/etc/accu/website.cfg'])
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'wjMB4SQxSY_3nuvYmpYyWg'
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+cfg = Config()
+password_checker = Checker(cfg['database']['host'], cfg['database']['password'])
+
+class Member:
+    def __init__(self, name=None):
+        self.name = name
+
+    def is_authenticated(self):
+        return self.name is not None
+
+    def is_active(self):
+        return self.is_authenticated()
+
+    def is_anonymous(self):
+        return not self.is_authenticated()
+
+    def get_id(self):
+        return self.name
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Member(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if password_checker.member(form.username.data, form.password.data):
+            user = Member(form.username.data)
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect('/journal/cvu')
+        else:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    logout_user()
+    return redirect('/')
+
+@app.route('/<string:page_name>')
+@login_required
+def render_static(page_name):
+    return render_template(page_name)
+
+if __name__ == '__main__':
+    app.run()
