1034550ed602e47d312c25b8fba9441036168e24
Jim Hague <jim.hague@acm.org>
2020-03-30 11:52:16 +0100

    Correct typo in makesite.sh.
    Really compress the output. Also set executable flag on script.

--- accu.org/makesite.sh
+++ accu.org/makesite.sh
@@ -3,4 +3,4 @@
 rm -rf public
 hugo -b https://newsite.accu.org
 tar -C content -c --exclude "*.html" journal/ | tar -C public -x
-tar -C public -cvf ../newsite.tar.gz .
+tar -C public -cvzf ../newsite.tar.gz .
